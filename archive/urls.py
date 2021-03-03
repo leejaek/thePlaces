d66f05689f5a1a607cac3b3a768f4077b9e4b4c7
leejaek <67036362+leejaek@users.noreply.github.com>
2021-03-03 14:26:05 +0900

    Add: Review CRUD 기능 구현 및 테스트 코드 작성

--- archive/urls.py
+++ archive/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import CheckInView, CheckInDeleteView #, ReviewView
+from .views import CheckInView, CheckInDeleteView, ReviewView, ReviewUpdateDeleteView
 
 urlpatterns = [
     path('/checkin/place/<int:place_pk>', CheckInView.as_view()),
-    path('/checkin/<int:checkin_pk>', CheckInDeleteView.as_view())
-#    path('/review/place/<int:place_pk>', ReviewView.as_view())
+    path('/checkin/<int:checkin_pk>', CheckInDeleteView.as_view()),
+    path('/review/place/<int:place_pk>', ReviewView.as_view()),
+    path('/review/<int:review_pk>', ReviewUpdateDeleteView.as_view())
 ]
 
--- archive/views.py
+++ archive/views.py
@@ -16,13 +16,14 @@ class CheckInView(View):
     def post(self, request, place_pk):
         try:
             user = request.user
-            
-            if CheckIn.objects.filter(user = user, place_id = place_pk, deleted_at__isnull = True).exists():
-                recent_checkin = CheckIn.objects.filter(user = user, place_id = place_pk, deleted_at__isnull = True).latest('created_at')
+            place = Place.objects.get(id = place_pk)
+
+            if CheckIn.objects.filter(user = user, place = place, deleted_at__isnull = True).exists():
+                recent_checkin = CheckIn.objects.filter(user = user, place = place, deleted_at__isnull = True).latest('created_at')
                 if (timezone.now() - recent_checkin.created_at).days == 0:
                     return JsonResponse({"message": "ALREADY_CHECKED_IN_TODAY"}, status = 401)
             
-            CheckIn.objects.create(user = user, place_id = place_pk)
+            CheckIn.objects.create(user = user, place = place)
             return JsonResponse({"message": "CHECKED_IN"}, status = 201)
 
         except Place.DoesNotExist:
@@ -52,11 +53,86 @@ class CheckInDeleteView(View):
             if delete_object.user == user:
                 delete_object.deleted_at = timezone.now()
                 delete_object.save()
-                return JsonResponse({"message": "DELETED"}, status = 204)
+                return JsonResponse({"message": "CHECK_IN_DELETED"}, status = 204)
             else:
                 return JsonResponse({"message": "UNAUTHORIZED"}, status = 401)
 
         except CheckIn.DoesNotExist:
             return JsonResponse({"message": "INVALID_CHECKIN"}, status = 400)
 
+class ReviewView(View):
+    @id_auth
+    def post(self, request, place_pk):
+        """해당 장소에 대한 리뷰 작성"""
+        try:
+            user = request.user
+            place = Place.objects.get(id = place_pk)
+            body = json.loads(request.body)['body']
 
+            Review.objects.create(user = user, place= place, body = body)
+            return JsonResponse({"message": "REVIEW_CREATED"}, status = 201)
+
+        except json.JSONDecodeError as e:
+            return JsonResponse({"message": f"{e}"}, status = 400)
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status = 400)
+        except Place.DoesNotExist:
+            return JsonResponse({"message": "INVALID_PLACE"}, status = 400)
+
+    def get(self, request, place_pk):
+        """해당 장소에 대한 로그인 유저의 리뷰 조회"""
+        try:
+            place = Place.objects.get(id = place_pk)
+            
+            reviews = Review.objects.select_related('user').filter(place = place, deleted_at__isnull = True)
+
+            result = [
+                        {
+                            "id": review.id,
+                            "user": review.user.nickname, 
+                            "body": review.body,
+                            "created_at": review.created_at.strftime('%Y-%m-%d')
+                        } for review in reviews
+                    ]
+
+            return JsonResponse({"result": result}, status = 200)
+        except Place.DoesNotExist:
+            return JsonResponse({"message": "INVALID_PLACE"}, status = 400)
+
+class ReviewUpdateDeleteView(View):
+    @id_auth
+    def patch(self, request, review_pk):
+        try:
+            user   = request.user
+            review = Review.objects.get(id = review_pk, deleted_at__isnull = True)
+            body   = json.loads(request.body)['body']
+
+            if review.user == user:
+                review.body = body
+                review.save()
+                return JsonResponse({"message": "REVIEW_UPDATED"}, status = 200)
+            else:
+                return JsonResponse({"message": "UNAUTHORIZAED"}, status = 401)
+
+        except json.JSONDecodeError as e:
+            return JsonResponse({"message": f"{e}"}, status = 400)
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status = 400)
+        except Review.DoesNotExist:
+            return JsonResponse({"message": "INVALID_REVIEW"}, status = 400)
+
+    @id_auth
+    def delete(self, request, review_pk):
+        try:
+            user   = request.user
+            review = Review.objects.get(id = review_pk, deleted_at__isnull = True)
+            
+            if review.user == user:
+                review.deleted_at = timezone.now()
+                review.save()
+                return JsonResponse({"message": "REVIEW_DELETED"}, status = 204)
+            else:
+                return JsonResponse({"message": "UNAUTHORIZED"}, status = 401)
+        
+        except Review.DoesNotExist:
+            return JsonResponse({"message": "INVALID_REVIEW"}, status = 400)
